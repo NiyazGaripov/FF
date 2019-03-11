9c93a1dc15f34b4b6ecf653caaebf3f365ec6746
Niyaz Garipov <Niayz_Indicus@mail.ru>
2019-03-11 10:13:56 +0300

    Adds script for tab

--- source/js/tab.js
+++ source/js/tab.js
@@ -1,17 +1,19 @@
-// 'use strict';
+'use strict';
 
-// (function () {
-//   $(document).redy(function() {
-//     $('.faq__item').on('click', function(evt) {
-//       evt.preventDefault();
+(function () {
+  $(document).ready(function() {
+    $('.faq__item-link').on('click', function(evt) {
+      evt.preventDefault();
 
-//       var item = $(this).closest('.faq__item'),
-//           contentItem = $('.faq__answers'),
-//           itemPosition = item.index();
+      var item = $(this).closest('.faq__item'),
+          contentItem = $('.faq__answers'),
+          itemPosition = item.index();
 
-//       contentItem.eq(itemPosition)
-//         .add(item)
-//         .addClass()
-//     })
-//   })
-// })();
+      contentItem.eq(itemPosition)
+        .add(item)
+        .addClass('active')
+        .siblings()
+        .removeClass('active');
+    })
+  })
+})();
