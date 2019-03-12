7c471ee0f8302349997eb3275ee3bbb47476f632
Niyaz Garipov <Niayz_Indicus@mail.ru>
2019-03-13 02:47:20 +0300

    Adds menu script

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,23 +1,42 @@
 'use strict';
 
 (function () {
-  var compositionLink = document.querySelectorAll('.menu__link');
-  var composition = document.querySelector('.composition');
-  var compositionClose = document.querySelectorAll('.composition__close');
+  $(document).ready(function() {
 
-  for (var i = 0; i < compositionLink.length; i++) {
-    compositionLink[i].addEventListener('click', function(evt) {
+    var $compositionLink = $('.menu__link'),
+        $composition = $('.composition'),
+        $compositionClose = $('.composition__close');
+
+    $compositionLink.on('click', function(evt) {
       evt.preventDefault();
 
-      composition.classList.add("composition--active");
-    });
-  }
+      $composition.addClass("composition--active");
+    })
 
-  for (var j = 0; j < compositionClose.length; j++) {
-    compositionClose[j].addEventListener("click", function(evt) {
+    $compositionClose .on('click', function(evt) {
       evt.preventDefault();
 
-      composition.classList.remove("composition--active");
-    });
-  }
+      $composition.removeClass("composition--active");
+    })
+
+  })
+  // var compositionLink = document.querySelectorAll('.menu__link');
+  // var composition = document.querySelector('.composition');
+  // var compositionClose = document.querySelectorAll('.composition__close');
+
+  // for (var i = 0; i < compositionLink.length; i++) {
+  //   compositionLink[i].addEventListener('click', function(evt) {
+  //     evt.preventDefault();
+
+  //     composition.classList.add("composition--active");
+  //   });
+  // }
+
+  // for (var j = 0; j < compositionClose.length; j++) {
+  //   compositionClose[j].addEventListener("click", function(evt) {
+  //     evt.preventDefault();
+
+  //     composition.classList.remove("composition--active");
+  //   });
+  // }
 })();
